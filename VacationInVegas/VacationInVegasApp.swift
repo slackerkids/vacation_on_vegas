7e31bc08a778693cff4e1658ea52803b3a03a157
slackerkids <145984578+slackerkids@users.noreply.github.com>
2024-10-25 22:45:45 +0500

    Initial Commit

---
+++ VacationInVegas/VacationInVegasApp.swift
@@ -0,0 +1,17 @@
+//
+//  VacationInVegasApp.swift
+//  VacationInVegas
+//
+//  Created by Sagidanov Bakhtiyar on 25.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct VacationInVegasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
